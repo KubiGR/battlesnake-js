51386e1a6470508300e5c2a2b0b93300cde0f167
KubiGR <adranidisb@gmail.com>
2022-07-17 23:34:33 +0300

    Remove suicide trigger

--- src/logic.js
+++ src/logic.js
@@ -218,11 +218,11 @@ export function move(gameState) {
     gameState.you.head.y
   );
 
-  responseNr++;
-  if (responseNr > 12) {
-    console.log("SUICIDE");
-    return { move: "up" };
-  }
+  // responseNr++;
+  // if (responseNr > 12) {
+  //   console.log("SUICIDE");
+  //   return { move: "up" };
+  // }
 
   const response = {
     move: direction,
@@ -230,4 +230,4 @@ export function move(gameState) {
   return response;
 }
 
-let responseNr = 0;
+// let responseNr = 0;
